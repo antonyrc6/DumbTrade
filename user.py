70af9d4cb7375fba3a5d46faac76c49d50fcd5b2
antony <ampaxmusic6@gmail.com>
2023-01-31 13:37:43 -0800

    change

---
+++ user.py
@@ -0,0 +1,20 @@
+import sqlite3
+conn = sqlite3.connect('user.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE customers (
+        first_name text,
+        last_name text,
+        email text,
+        password text
+    )""")
+
+# c.execute("INSERT INTO user VALUES ('Antony', 'Yu', 'antony@gmail.com', 1234'")
+
+# print("committed")
+
+conn.commit()
+
+conn.close()
+
